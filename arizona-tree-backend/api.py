18719e0b9499b2af645c00adcce40996ee91b4fe
Smit <smitdev2553@gmail.com>
2024-09-29 09:44:26 -0700

    Refactor file paths and optimize image processing in canopy.py

--- arizona-tree-backend/api.py
+++ arizona-tree-backend/api.py
@@ -1,6 +1,7 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from canopy import *
+from io import BytesIO
+from PIL import Image
 
 app = FastAPI()
 
@@ -10,7 +11,16 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/canopy/{file_path}")
-def run_canopy(file_path):
-    canapy_coverage(file_path)
+@app.post("/canopy")
+async def run_canopy(file: UploadFile = File(...)):
+    if file.content_type != "image/png":
+        return {"error": "Only PNG files are allowed."}
+
+    # Read the image from the uploaded file
+    image_data = await file.read()
+    image = Image.open(BytesIO(image_data))
+
+    # Run the canopy_coverage function with the image
+    canopy_coverage(image)
+
     return {"It worked!": "Success!"}
